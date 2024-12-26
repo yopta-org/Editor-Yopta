be2fd48257f222d184c46038f5a4d73538d2af38
Vaishnav Deore <86405648+Waishnav@users.noreply.github.com>
2024-12-27 03:15:35 +0530

    Update deserialize.ts (#415)

--- packages/core/exports/src/markdown/deserialize.ts
+++ packages/core/exports/src/markdown/deserialize.ts
@@ -3,6 +3,30 @@ import { marked } from 'marked';
 import { deserializeHTML } from '../html/deserialize';
 
 export function deserializeMarkdown(editor: YooEditor, markdown: string): YooptaContentValue {
+  const imageExtension = {
+	  name: 'image',
+	  level: 'block',
+	  start(src: string) {
+		  return src.match(/!\[/)?.index;
+	  },
+	  tokenizer(src: string) {
+		  const rule = /^!\[(.*?)\]\((.*?)\)/;
+		  const match = rule.exec(src);
+		  if (match) {
+			  return {
+				  type: 'image',
+				  raw: match[0],
+				  alt: match[1],
+				  href: match[2]
+			  };
+		  }
+		  return;
+	  },
+	  renderer(token: any) {
+		  return `<img alt="${token.alt}" src="${token.href}">`;
+	  }
+  };
+  marked.use({ extensions: [imageExtension] });
   const html = marked.parse(markdown, { gfm: true, breaks: true, pedantic: false }) as string;
   return deserializeHTML(editor, html);
 }
