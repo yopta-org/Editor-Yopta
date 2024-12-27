96fc42e95c5ee312ae64b0c5016479fed75bd245
Darginec05 <devopsbanda@gmail.com>
2024-12-27 21:45:08 +0100

    fix deploy

--- web/next-example/src/components/landing/GithubSponsors/GithubSponsors.tsx
+++ web/next-example/src/components/landing/GithubSponsors/GithubSponsors.tsx
@@ -11,32 +11,36 @@ export const GithubSponsors = ({ sponsors }: Props) => {
   return (
     <div>
       <h4 className="text-center mt-4 text-lg font-semibold">Sponsors 💖</h4>
-      <div className="w-full flex-wrap flex justify-center py-2">
-        {sponsors.current.map((sponsor) => (
-          <a
-            key={sponsor.username}
-            href={`https://github.com/${sponsor.username}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center justify-center w-18 h-18 rounded-full shadow-lg m-2"
-          >
-            <img src={sponsor.avatar} alt={`${sponsor.username} avatar`} className="w-14 h-14 rounded-full" />
-          </a>
-        ))}
-      </div>
-      <div className="w-full flex-wrap flex justify-center">
-        {sponsors.past.map((sponsor) => (
-          <a
-            key={sponsor.username}
-            href={`https://github.com/${sponsor.username}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center justify-center w-18 h-18 rounded-full shadow-lg m-2"
-          >
-            <img src={sponsor.avatar} alt={`${sponsor.username} avatar`} className="w-14 h-14 rounded-full" />
-          </a>
-        ))}
-      </div>
+      {sponsors?.current && (
+        <div className="w-full flex-wrap flex justify-center py-2">
+          {sponsors?.current?.map((sponsor) => (
+            <a
+              key={sponsor.username}
+              href={`https://github.com/${sponsor.username}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-center w-18 h-18 rounded-full shadow-lg m-2"
+            >
+              <img src={sponsor.avatar} alt={`${sponsor.username} avatar`} className="w-14 h-14 rounded-full" />
+            </a>
+          ))}
+        </div>
+      )}
+      {sponsors?.past && (
+        <div className="w-full flex-wrap flex justify-center">
+          {sponsors?.past?.map((sponsor) => (
+            <a
+              key={sponsor.username}
+              href={`https://github.com/${sponsor.username}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-center w-18 h-18 rounded-full shadow-lg m-2"
+            >
+              <img src={sponsor.avatar} alt={`${sponsor.username} avatar`} className="w-14 h-14 rounded-full" />
+            </a>
+          ))}
+        </div>
+      )}
       <div className="w-full flex-wrap flex justify-center">
         <a
           href="https://github.com/sponsors/Darginec05"
